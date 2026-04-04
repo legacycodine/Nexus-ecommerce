9f39994cf2ddefdaa1828f18873bbd5aa8311694
legacycodine <adebimpee842@gmail.com>
2026-04-04 23:42:25 +0100

    pages

---
+++ Frontend/ecommerce-frontend/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import MainLayout from "../layouts/MainLayout";
+
+const Home = () => {
+  return (
+    <MainLayout>
+
+      <h1 className="text-3xl font-bold">
+        Latest Products
+      </h1>
+
+    </MainLayout>
+  );
+};
+
+export default Home;
