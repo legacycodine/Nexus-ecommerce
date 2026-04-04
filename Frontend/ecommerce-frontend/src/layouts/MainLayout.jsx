430d84786feac3a237d58dd3981ca1d26c0ee539
legacycodine <adebimpee842@gmail.com>
2026-04-04 23:39:46 +0100

    layouts

---
+++ Frontend/ecommerce-frontend/src/layouts/MainLayout.jsx
@@ -0,0 +1,20 @@
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
+
+const MainLayout = ({ children }) => {
+  return (
+    <div className="flex flex-col min-h-screen">
+
+      <Navbar />
+
+      <main className="flex-grow container mx-auto px-4 py-6">
+        {children}
+      </main>
+
+      <Footer />
+
+    </div>
+  );
+};
+
+export default MainLayout;
