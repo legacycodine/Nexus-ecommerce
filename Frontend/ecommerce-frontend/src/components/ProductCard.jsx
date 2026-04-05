1c55a7fa3c22a6a0d623912a70d5db6c981a904c
legacycodine <adebimpee842@gmail.com>
2026-04-05 16:42:29 +0100

    frontend

---
+++ Frontend/ecommerce-frontend/src/components/ProductCard.jsx
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+
+const ProductCard = ({ product }) => {
+  return (
+    <div className="bg-white rounded-xl shadow hover:shadow-lg transition">
+
+      <Link to={`/product/${product._id}`}>
+        <img
+          src={product.image}
+          alt={product.name}
+          className="rounded-t-xl h-48 w-full object-cover"
+        />
+      </Link>
+
+      <div className="p-4">
+
+        <h2 className="font-semibold text-lg mb-2">
+          {product.name}
+        </h2>
+
+        <p className="text-gray-600 mb-3">
+          ${product.price}
+        </p>
+
+        <button className="w-full bg-black text-white py-2 rounded-lg hover:bg-gray-800">
+          Add to Cart
+        </button>
+
+      </div>
+
+    </div>
+  );
+};
+
+export default ProductCard;
