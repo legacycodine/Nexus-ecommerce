b99d4319462779980c01b386562df38698bcd43f
legacycodine <adebimpee842@gmail.com>
2026-04-05 16:43:28 +0100

    frontnend

--- Frontend/ecommerce-frontend/src/pages/Home.jsx
+++ Frontend/ecommerce-frontend/src/pages/Home.jsx
@@ -1,12 +1,37 @@
+import { useEffect, useState } from "react";
 import MainLayout from "../layouts/MainLayout";
+import ProductCard from "../components/ProductCard";
+import { fetchProducts } from "../api/productApi";
 
 const Home = () => {
+
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+
+    const loadProducts = async () => {
+      const data = await fetchProducts();
+      setProducts(data);
+    };
+
+    loadProducts();
+
+  }, []);
+
   return (
     <MainLayout>
 
       <h1 className="text-3xl font-bold mb-6">
         Latest Products
       </h1>
+
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+
+        {products.map((product) => (
+          <ProductCard key={product._id} product={product} />
+        ))}
+
+      </div>
 
     </MainLayout>
   );
