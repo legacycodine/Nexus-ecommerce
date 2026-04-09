e85c23481db39a3aec0dff9bf16f18412fcdcf44
legacycodine <adebimpee842@gmail.com>
2026-04-09 17:43:34 +0100

    cors

--- server/server.js
+++ server/server.js
@@ -14,12 +14,13 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(
-  cors({
-    origin: process.env.CLIENT_URL,
-    credentials: true,
-  })
-);
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:3000"
+  ],
+  credentials: true
+}));
 
 
 // ROUTES
